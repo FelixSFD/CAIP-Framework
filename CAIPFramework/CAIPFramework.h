d4af064401883f8179d36f99f764d1e86204afec
Felix Deil <felix.deil@student.uni-augsburg.de>
2015-12-22 00:43:30 +0100

    Initial Commit

---
+++ CAIPFramework/CAIPFramework.h
@@ -0,0 +1,19 @@
+//
+//  CAIPFramework.h
+//  CAIPFramework
+//
+//  Created by Felix Deil on 22.12.15.
+//  Copyright © 2015 Felix Deil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CAIPFramework.
+FOUNDATION_EXPORT double CAIPFrameworkVersionNumber;
+
+//! Project version string for CAIPFramework.
+FOUNDATION_EXPORT const unsigned char CAIPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAIPFramework/PublicHeader.h>
+
+
